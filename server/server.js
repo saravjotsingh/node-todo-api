bcbadf7e352863a4d75794518ce3e89964a6d5ff
saravjotsingh <saravjotsaini@gmail.com>
2017-06-09 15:40:01 +0530

    Add Url Params

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var app = express();
 app.use(bodyParser.json());
 
 
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo.js');
+var {Todo } = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 
@@ -25,13 +25,36 @@ todo.save().then((docs)=>{
 });
 });
 
+
 app.get('/todos',(req,res)=>{
    Todo.find().then((todos)=>{
        res.send({todos});
    },(e)=>{
        res.status(400).send(e);
    }); 
+    
+    
 });
+
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    
+    if(!ObjectID.isValid(id)){
+     return res.status(404).send('Please Enter a Valid ID');
+        //console.log('hello');
+    }
+    
+    Todo.findById({
+        _id:id
+    }).then((todos)=>{
+  
+      if(!todos){
+          return res.status(400).send('Enter Coreect ID');
+      }  res.status(200).send(JSON.stringify(todos,undefined,2));
+    }).catch((e)=>{res.status(404).send()});
+    
+});
+
 
 app.listen(3000,()=>{
     console.log('listening on port 3000');
