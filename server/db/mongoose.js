21d9ccb2a01649688938db8a9112ab1b04552ca8
saravjotsingh <saravjotsaini@gmail.com>
2017-06-13 22:02:27 +0530

    modified

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+//
+//var db = {
+//    mlab : "mongodb://<todoapi>:<saravjot>@ds125262.mlab.com:25262/todoapi",
+//    local : "mongodb://localhost:27017/TodoApp"
+//}
 
-var db = {
-    mlab : "mongodb://<todoapi>:<saravjot>@ds125262.mlab.com:25262/todoapi",
-    local : "mongodb://localhost:27017/TodoApp"
-}
-
-mongoose.connect(process.env.PORT ? db.mlab : db.local);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
 
 module.exports = {mongoose}
