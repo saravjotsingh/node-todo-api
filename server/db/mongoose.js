4e7d90e7fcf57f87eb1a88c9821a6a2c3d7dc94a
saravjotsingh <saravjotsaini@gmail.com>
2017-06-13 21:49:38 +0530

    commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,9 +3,9 @@ mongoose.Promise = global.Promise;
 
 var db = {
     mlab : "mongodb://<todoapi>:<saravjot>@ds125262.mlab.com:25262/todoapi",
-    local : 'mongodb://localhost:27017/TodoApp'
+    local : "mongodb://localhost:27017/TodoApp"
 }
 
-mongoose.connect("mongodb://<todoapi>:<saravjot>@ds125262.mlab.com:25262/todoapi");
+mongoose.connect(process.env.PORT ? db.mlab : db.local);
 
 module.exports = {mongoose}
